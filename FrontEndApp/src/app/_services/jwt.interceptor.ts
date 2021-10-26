1c3df550a7bf8bf026c1f30ee37f5352e02c30d0
Neph <philippe.balleydier@gmail.com>
2021-10-26 21:22:12 +0200

    auto reroute if user is not logged in

--- FrontEndApp/src/app/_services/jwt.interceptor.ts
+++ FrontEndApp/src/app/_services/jwt.interceptor.ts
@@ -1,26 +1,40 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 
 import { UserService } from './user.service';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor
 {
-	constructor(private userService: UserService) { }
+	constructor(private userService: UserService, private router: Router) { }
 
 	intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>
   {
 		const currentUser = this.userService.currentUserValue;
 		if (currentUser && currentUser.token)
-    {
+		{
 			request = request.clone({
-        setHeaders: {
+				setHeaders: {
 					Authorization: `Bearer ${currentUser.token}`
-				}
+				}	
 			});
 		}
 
-		return next.handle(request);
+		return next.handle(request).pipe(catchError(err =>
+		{
+			if (err.status === 401)
+			{
+				this.userService.logout();
+				this.router.navigate(['/login']);
+			}
+			else if (err.status === 0 && err.name === "HttpErrorResponse")
+			{
+				this.router.navigate(['/error']);
+			}
+			return throwError(err);
+		}))
 	}
 }
--- FrontEndApp/src/app/app.component.ts
+++ FrontEndApp/src/app/app.component.ts
@@ -9,23 +9,23 @@ import { Router } from '@angular/router';
 })
 export class AppComponent
 {
-  title: string = 'TheCompany';
-  guest: boolean = true;
+	title: string = 'TheCompany';
+	guest: boolean = true;
 
-  constructor(
-    private userService: UserService,
-    private router: Router
-  )
-  {
-    userService.currentUser.subscribe(value =>
-    {
-      this.guest = value.id === '';
-    });
-  }
+	constructor(
+		private userService: UserService,
+		private router: Router
+	)
+	{
+		userService.currentUser.subscribe(value =>
+		{
+			this.guest = (value.id === undefined || value.id === '');
+		});
+	}
 
-  logout()
-  {
-    this.userService.logout();
-    this.router.navigate(['/login']);
-  }
+	logout()
+	{
+		this.userService.logout();
+		this.router.navigate(['/login']);
+	}
 }
