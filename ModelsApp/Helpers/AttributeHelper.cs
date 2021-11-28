ed50e683cd5886e9e617fd8ecf8071f34845af68
Neph <philippe.balleydier@gmail.com>
2021-11-28 15:42:17 +0100

    provide helpers to check class and property attributes

---
+++ ModelsApp/Helpers/AttributeHelper.cs
@@ -0,0 +1,66 @@
+﻿using ModelsApp.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace ModelsApp.Helpers
+{
+	public static class AttributeHelper
+	{
+		public static bool CheckAttribute<T>(Type type)
+		{
+			System.Attribute[] attrs = System.Attribute.GetCustomAttributes(type);
+			foreach (System.Attribute attr in attrs)
+			{
+				if (attr is T)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public static bool CheckAttribute<T>(Type type, string fieldName)
+		{
+			PropertyInfo property = type.GetProperty(fieldName, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+			return CheckAttribute<T>(type, property);
+		}
+
+		public static bool CheckAttribute<T>(Type type, PropertyInfo property)
+		{
+			System.Attribute[] attrs = System.Attribute.GetCustomAttributes(property);
+			foreach (System.Attribute attr in attrs)
+			{
+				if (attr is T)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public static List<PropertyInfo> GetAuthorizedProperties<T>(Type type)
+		{
+			List<PropertyInfo> result = new List<PropertyInfo>();
+			PropertyInfo[] properties = type.GetProperties();
+			foreach (PropertyInfo property in properties)
+			{
+				if (AttributeHelper.CheckAttribute<Extractable>(type, property))
+					result.Add(property);
+			}
+			return result;
+		}
+
+		public static List<string> GetAuthorizedPropertiesAsString<T>(Type type)
+		{
+			List<string> result = new List<string>();
+			List<PropertyInfo> properties = GetAuthorizedProperties<T>(type);
+			foreach (PropertyInfo property in properties)
+			{
+				result.Add(property.Name);
+			}
+			return result;
+		}
+	}
+}
