5b778968bc794a22085e3fd0f0a91496ca5df32d
Neph <philippe.balleydier@gmail.com>
2021-12-07 19:41:18 +0100

    ctrl k d

--- BackEndApp/Program.cs
+++ BackEndApp/Program.cs
@@ -10,31 +10,31 @@ using System.Reflection;
 
 namespace BackEndApp
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			Directory.SetCurrentDirectory(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
-			CreateHostBuilder(args).Build().Run();
-		}
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Directory.SetCurrentDirectory(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
+            CreateHostBuilder(args).Build().Run();
+        }
 
-		public static IHostBuilder CreateHostBuilder(string[] args) =>
-			Host.CreateDefaultBuilder(args)
-				.ConfigureWebHostDefaults(webBuilder =>
-				{
-					webBuilder.UseStartup<Startup>();
-				}).ConfigureLogging(logging =>
-				{
-					GlobalDiagnosticsContext.Set("AppName", "BackEndApp");
-					logging.ClearProviders();
-					logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
-				})
-				.UseNLog()
-				.ConfigureAppConfiguration(config =>
-				{
-					config.SetBasePath(Directory.GetCurrentDirectory());
-					config.AddJsonFile("appSettings.json", false);
-					config.AddJsonFile("appSettings.Development.json", true);
-				});
-	}
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                }).ConfigureLogging(logging =>
+                {
+                    GlobalDiagnosticsContext.Set("AppName", "BackEndApp");
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                })
+                .UseNLog()
+                .ConfigureAppConfiguration(config =>
+                {
+                    config.SetBasePath(Directory.GetCurrentDirectory());
+                    config.AddJsonFile("appSettings.json", false);
+                    config.AddJsonFile("appSettings.Development.json", true);
+                });
+    }
 }
--- BackEndApp/Startup.cs
+++ BackEndApp/Startup.cs
@@ -7,88 +7,87 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
-using StorageApp;
 using System.Text;
 
 namespace BackEndApp
 {
-	public class Startup
-	{
-		public Startup(IConfiguration configuration)
-		{
-			Configuration = configuration;
-		}
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-		public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
-		public void ConfigureServices(IServiceCollection services)
-		{
-			services.AddSingleton<IConfiguration>(Configuration);
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IConfiguration>(Configuration);
 
-			services.AddControllers();
+            services.AddControllers();
 
-			services.AddCors(options =>
-			{
-				options.AddPolicy("AllowAll", p =>
-				{
-					p.AllowAnyOrigin()
-					.AllowAnyHeader()
-					.AllowAnyMethod();
-				});
-			});
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", p =>
+                {
+                    p.AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                });
+            });
 
-			var key = Encoding.UTF8.GetBytes(Configuration["AppSettings:AuthenticationSecret"]);
-			services.AddAuthentication(x =>
-			{
-				x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-				x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-			})
-			.AddJwtBearer(x =>
-			{
-				x.RequireHttpsMetadata = false;
-				x.SaveToken = true;
-				x.TokenValidationParameters = new TokenValidationParameters
-				{
-					ValidateIssuerSigningKey = true,
-					IssuerSigningKey = new SymmetricSecurityKey(key),
-					ValidateIssuer = false,
-					ValidateAudience = false
-				};
-			});
+            var key = Encoding.UTF8.GetBytes(Configuration["AppSettings:AuthenticationSecret"]);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
 
-			services.AddControllers(obj =>
-			{
-				var policy = new AuthorizationPolicyBuilder()
-					.RequireAuthenticatedUser()
-					.Build();
-				obj.Filters.Add(new AuthorizeFilter(policy));
-			});
-		}
+            services.AddControllers(obj =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+                obj.Filters.Add(new AuthorizeFilter(policy));
+            });
+        }
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
-			app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
-			app.UseRouting();
+            app.UseRouting();
 
-			app.UseCors("AllowAll");
+            app.UseCors("AllowAll");
 
-			app.UseAuthentication();
-			app.UseAuthorization();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
-			app.UseEndpoints(endpoints =>
-			{
-				endpoints.MapControllerRoute(
-					name: "default",
-					pattern: "{controller}/{action}/{id?}");
-			});
-		}
-	}
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action}/{id?}");
+            });
+        }
+    }
 }
