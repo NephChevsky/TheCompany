4a1b3822c966253705d5df862013ad9bd97fbc13
Neph <philippe.balleydier@gmail.com>
2021-12-10 16:47:43 +0100

    declaring variable to allow to build

--- FrontEndApp/src/app/line-items/line-items.component.ts
+++ FrontEndApp/src/app/line-items/line-items.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-line-items',
-  templateUrl: './line-items.component.html',
-  styleUrls: ['./line-items.component.scss']
+	selector: 'app-line-items',
+	templateUrl: './line-items.component.html',
+	styleUrls: ['./line-items.component.scss']
 })
-export class LineItemsComponent implements OnInit {
+export class LineItemsComponent implements OnInit
+{
+	public dataSource: string = "LineItemDefinition";
+	public linkField: string = "Id";
+	public linkRoute: string = "LineItems/Show/"
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void
+	{
+	}
 
 }
