f2508956803c31293b3adb728396263a1c996112
Neph <philippe.balleydier@gmail.com>
2021-12-10 15:36:33 +0100

    use invoice details to generate pdf

--- PdfApp/PdfEngine.cs
+++ PdfApp/PdfEngine.cs
@@ -1,33 +1,91 @@
 ﻿using PdfSharpCore.Drawing;
 using PdfSharpCore.Pdf;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Formats.Png;
 using System;
+using System.Drawing.Imaging;
 using System.IO;
 
 namespace PdfApp
 {
     public class PdfEngine
     {
-        public PdfDocument Document {get; set;}
-        public PdfPage Page { get; set; }
+        private PdfDocument _Document;
+        private PdfPage _Page;
+        private XGraphics _Gfx;
 
-        public bool CreateDocument()
+        private PdfDocument Document {
+            get
+            {
+                if (_Document == null)
+                {
+                    throw new Exception("No document");
+                }
+                return _Document;
+            }
+            set
+            {
+                _Document = value;
+            }
+        }
+        private PdfPage Page
+        {
+            get
+            {
+                if (_Page == null)
+                {
+                    throw new Exception("No page selected");
+                }
+                return _Page;
+            }
+            set
+            {
+                _Page = value;
+            }
+        }
+        
+        private XGraphics Gfx {
+            get
+            {
+                if (_Gfx == null)
+                {
+                    _Gfx = XGraphics.FromPdfPage(Page);
+                }
+                return _Gfx;
+            }
+            set
+            {
+                _Gfx = value;
+            }
+        }
+
+        public void CreateDocument()
         {
             Document = new PdfDocument();
-            return true;
         }
 
-        public bool InsertPage(int index)
+        public void InsertPage(int index)
         {
             Page = Document.InsertPage(index);
-            return true;
+            Gfx = null;
         }
 
-        public bool AddText(string text, double x, double y)
+        public void AddText(string text, double x, double y)
         {
-            XGraphics gfx = XGraphics.FromPdfPage(Page);
             XFont font = new XFont("Arial", 14, XFontStyle.Regular);
-            gfx.DrawString(text, font, XBrushes.Black, new XRect(x, y, Page.Width, Page.Height), XStringFormats.TopLeft);
-            return true;
+            Gfx.DrawString(text, font, XBrushes.Black, new XRect(x, y, Page.Width, Page.Height), XStringFormats.TopLeft);
+        }
+
+        public void InsertImage(MemoryStream stream, double x, double y, double width, double height)
+        {
+            string tempFileName = Path.GetTempFileName() + ".png";
+            FileStream file = new FileStream(tempFileName, FileMode.Create);
+            stream.WriteTo(file);
+            file.Close();
+            XImage image = XImage.FromFile(tempFileName);
+            Gfx.DrawImage(image, x, y, width, height);
+            File.Delete(tempFileName);
         }
 
         public MemoryStream ToStream()
--- WorkerServiceApp/GenerateDocument.cs
+++ WorkerServiceApp/GenerateDocument.cs
@@ -39,6 +39,11 @@ namespace WorkerServiceApp
         {
             _logger.LogInformation(string.Concat("Function \"GenerateDocument\" started (Id: ", AppId, ")"));
 
+            bool debugMode = false;
+#if DEBUG
+            debugMode = true;
+#endif
+
             while (!stoppingToken.IsCancellationRequested)
             {
                 using (var db = new TheCompanyDbContext())
@@ -46,44 +51,101 @@ namespace WorkerServiceApp
                     bool stop = false;
                     while (!stop && !stoppingToken.IsCancellationRequested)
                     {
-                        Invoice invoice = db.Invoices.Where(x => x.ShouldBeGenerated == true && x.IsGenerated == false && string.IsNullOrEmpty(x.LockedBy)).FirstOrDefault();
-                        if (invoice != null)
+                        Invoice dbInvoice = db.Invoices.Where(x => x.ShouldBeGenerated == true && x.IsGenerated == false && string.IsNullOrEmpty(x.LockedBy)).FirstOrDefault();
+                        if (dbInvoice != null)
                         {
-                            _logger.LogInformation(string.Concat("Start processing invoice " + invoice.Id.ToString()));
-                            invoice.LockedBy = string.Concat("GenerateDocument-", AppId);
+                            _logger.LogInformation(string.Concat("Start processing invoice " + dbInvoice.Id.ToString()));
+                            dbInvoice.LockedBy = string.Concat("GenerateDocument-", AppId);
                             db.SaveChanges();
 
+                            Storage storage = new Storage(Configuration.GetSection("Storage"), dbInvoice.Owner);
+
                             PdfEngine pdfEngine = new PdfEngine();
-                            if (!pdfEngine.CreateDocument())
+                            pdfEngine.CreateDocument();
+                            pdfEngine.InsertPage(0);
+
+                            int y = 20;
+
+                            Company dbCompany = db.Companies.Where(x => x.Owner == dbInvoice.Owner).SingleOrDefault();
+                            if (dbCompany != null)
                             {
-                                throw new Exception("Couldn't generate pdf file");
+                                MemoryStream logo;
+                                if (!storage.GetFile(dbCompany.Logo, out logo))
+                                {
+                                    throw new Exception("Couldn't retrieve logo");
+                                }
+                                pdfEngine.InsertImage(logo, 20, y, 162, 127);
                             }
-                            if (!pdfEngine.InsertPage(0))
+
+                            pdfEngine.AddText("Invoice N°: " + dbInvoice.InvoiceNumber, 325, y+20);
+                            pdfEngine.AddText("Creation Date: " + dbInvoice.CreationDateTime.ToString("yyyy-MM-dd"), 325, y+40);
+
+                            y = 200;
+                            pdfEngine.AddText(dbCompany.Name, 20, y);
+                            y+=20;
+                            List<string> addressLines = dbCompany.Address.Split("\n").ToList();
+                            addressLines.ForEach(line =>
                             {
-                                throw new Exception("Couldn't insert page in pdf file");
-                            }
-                            if (!pdfEngine.AddText("test", 20, 20))
+                                pdfEngine.AddText(line, 20, y);
+                                y+=20;
+                            });
+                            
+                            pdfEngine.AddText("Phone: " + dbCompany.PhoneNumber, 20, y);
+                            pdfEngine.AddText("Mobile: " + dbCompany.MobilePhoneNumber, 20, y+20);
+                            pdfEngine.AddText("SIRET: " + dbCompany.Siret, 20, y+40);
+
+                            if (dbInvoice.CustomerId != Guid.Empty)
                             {
-                                throw new Exception("Couldn't add text in pdf file");
+                                // TODO: write customer info
                             }
+
+                            y+=100;
+                            pdfEngine.AddText("Reference", 20, y);
+                            pdfEngine.AddText("Description", 100, y);
+                            pdfEngine.AddText("Quantity", 350, y);
+                            pdfEngine.AddText("UnitaryPrice", 420, y);
+                            pdfEngine.AddText("Price", 510, y);
+
+                            y+=30;
+                            double total = 0;
+                            List<InvoiceLineItem> lineItems = db.InvoiceLineItems.Where(x => x.Owner == dbInvoice.Owner && x.InvoiceId == dbInvoice.Id).ToList();
+                            lineItems.ForEach(item =>
+                            {
+                                pdfEngine.AddText(item.Reference, 20, y );
+                                pdfEngine.AddText(item.Description, 100, y);
+                                pdfEngine.AddText(string.Format("{0:N2}", item.Quantity), 350, y);
+                                pdfEngine.AddText(string.Format("{0:N2}", item.UnitaryPrice), 420, y);
+                                pdfEngine.AddText(string.Format("{0:N2}", item.Price), 510, y);
+                                y+=20;
+                                total += item.Price;
+                            });
+
+                            y+=100;
+                            pdfEngine.AddText("Total: " + string.Format("{0:N2}", total) + " €", 420, y);
 
                             MemoryStream output = pdfEngine.ToStream();
-
-                            Storage storage = new Storage(Configuration.GetSection("Storage"), invoice.Owner);
                             Guid fileId;
                             if (!storage.CreateFile(output, out fileId))
                             {
                                 throw new Exception("Couldn't create file " + fileId.ToString());
                             }
 
-                            invoice.FileId = fileId;
-                            invoice.FileName = "Invoice" + invoice.InvoiceNumber + ".pdf";
-                            invoice.FileSize = output.Length;
-                            invoice.LockedBy = null;
-                            invoice.IsGenerated = true;
-                            invoice.GenerationDateTime = DateTime.Now;
+                            if (debugMode)
+                            {
+                                string executingAssembly = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                                FileStream file = new FileStream(Path.Combine(executingAssembly, "generated.pdf"), FileMode.OpenOrCreate);
+                                output.WriteTo(file);
+                                file.Close();
+                            }
+
+                            dbInvoice.FileId = fileId;
+                            dbInvoice.FileName = "Invoice" + dbInvoice.InvoiceNumber + ".pdf";
+                            dbInvoice.FileSize = output.Length;
+                            dbInvoice.LockedBy = null;
+                            dbInvoice.IsGenerated = true;
+                            dbInvoice.GenerationDateTime = DateTime.Now;
                             db.SaveChanges();
-                            _logger.LogInformation(string.Concat("Invoice " + invoice.Id.ToString() + " has been processed"));
+                            _logger.LogInformation(string.Concat("Invoice " + dbInvoice.Id.ToString() + " has been processed"));
                         }
                         else
                         {
